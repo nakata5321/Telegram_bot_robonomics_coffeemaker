f3802bbbc4ae0b2e1dab18bc467094ca9d9574d0
nakata5321 <makar5321@gmail.com>
2021-10-27 19:21:37 +0300

    refactor to make modul

---
+++ config/config.py
@@ -0,0 +1,21 @@
+import typing as tp
+import logging
+import yaml
+
+
+def read_config() -> tp.Dict[str, str]:
+
+    config_path = 'config.yaml'
+    logging.debug(config_path)
+
+    try:
+        with open(config_path) as f:
+            content = f.read()
+            config = yaml.load(content, Loader=yaml.FullLoader)
+            logging.debug(f"Configuration dict: {content}")
+            return config
+    except Exception as e:
+        while True:
+            logging.error("Error in configuration file!")
+            logging.error(e)
+            exit()
--- main.py
+++ main.py
@@ -1,26 +1,6 @@
-import typing as tp
+from lib.telegram_bot import bot_sent_message
+from config.config import read_config
 import logging
-import yaml
-import os
-from scripts.telegram_bot import bot_sent_message
-
-
-def read_config(path: str) -> tp.Dict[str, str]:
-
-    config_path = path + 'config/config.yaml'
-    logging.debug(config_path)
-
-    try:
-        with open(config_path) as f:
-            content = f.read()
-            config = yaml.load(content, Loader=yaml.FullLoader)
-            logging.debug(f"Configuration dict: {content}")
-            return config
-    except Exception as e:
-        while True:
-            logging.error("Error in configuration file!")
-            logging.error(e)
-            exit()
 
 
 def main() -> None:
@@ -30,9 +10,7 @@ def main() -> None:
         format="%(asctime)s %(levelname)s: %(message)s",
     )
     logging.info("reading config file")
-    dirname = os.path.dirname(__file__)
-    logging.info(dirname)
-    config = read_config(dirname)
+    config = read_config()
 
     logging.info("send message to coffee delivers")
     bot_sent_message(config["bot_token"], config["bot_chat_id"], "misc/message.ogg")
